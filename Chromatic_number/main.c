6d216bd2fcf407903d8f342ab35ca5187782c122
Bivolan Cristina <you@example.com>
2018-06-03 19:12:15 +0300

    Adding comm

--- Chromatic_number/main.c
+++ Chromatic_number/main.c
@@ -1,4 +1,4 @@
-/// The minimum number of colors needed to colors each node in an undirected graph
+/// The minimum number of colors needed to colors each node in an undirected graph such that no two adjacent vertices share the same color.
 ///
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,10 +7,11 @@
 
 int main(){
 
-    struct a_graph *graph;
+    struct a_graph *graph;///\var *graph -a graph of type struct a_graph
 
-    int choose_algorithm;
-    graph=calloc(1, sizeof(struct a_graph));
+    int choose_algorithm;///\var choose_algorithm for chosing which algorithm to use
+
+    graph=calloc(1, sizeof(struct a_graph));///Allocate memory dynamically to the graph
     printf("\n");
     printf("Choose how to read graph :\n");
     printf("1.From keyboard\n");
@@ -25,7 +26,7 @@ int main(){
             init_graph_file(graph);
 
     print_adj_matrix(graph);
-
+     ///Solve chromatic number with one of the algorithms by choice.
     printf("\n");
     printf("WChoose which algorithm to use : \n");
     printf("1.Greedy Algorithm\n");
@@ -37,9 +38,9 @@ int main(){
     }else if(choose_algorithm == 2){
         chromatic_number_backtracking(graph, graph->no_nodes);
     }
+    ///Free memory for adjacent matrix, graph.
     free(graph->adj_matrix);;
     free(graph);
-
 
     return 0;
 }
